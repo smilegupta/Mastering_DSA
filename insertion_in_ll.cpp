5e5e6bbfb4b72a633a860fc2776a5d3b43d5c105
Smile Gupta <38321072+smilegupta3012@users.noreply.github.com>
2019-10-29 11:36:52 +0530

    Add files via upload

---
+++ insertion_in_ll.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct Node{
+	int data;
+	struct Node *next;
+}*first=NULL;
+void create(int A[], int n)
+{
+	struct Node *t, *last;
+	first=new Node;
+	first->data=A[0];
+	first->next=NULL;
+	last=first;
+	for(int i=1; i<n;i++)
+	{ 
+		t=new Node;
+		t->data=A[i];
+		t->next=NULL;
+		last->next=t;
+		last=t;	
+	}
+}
+
+void insert(struct Node *p, int val, int pos)
+{
+	struct Node *t =new Node;
+	int count=1;
+	if(pos==0)
+	{
+	t->data=val;
+	t->next=first;
+	first=t;
+	}
+	else{
+		while(p!=NULL)
+	{
+	if(pos==count)
+	{
+	t->data=val;
+	t->next=p->next;
+	p->next=t;
+	}
+	p=p->next;
+	count++;
+	}
+	}
+	
+	
+}
+void Display(struct Node *p)
+{
+	while(p!=NULL)
+	{
+		cout<<p->data<<" ";
+		p=p->next;
+	}
+}
+int main(){
+	int A[]={10,20,30,40,50};
+	int n=sizeof(A)/sizeof(A[0]);
+	create(A,n);
+	insert(first,0,5);
+	Display(first);
+
+	
+}
